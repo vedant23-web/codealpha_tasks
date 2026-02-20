ff59a3c0498b39eab21d012020c80e30bbaf0c90
Vedant Ingole <165572936+vedant23-web@users.noreply.github.com>
2026-02-20 14:36:46 +0530

    Update CodeAlpha_cgpa_calculator.cpp

--- CodeAlpha_cgpa_calculator.cpp
+++ CodeAlpha_cgpa_calculator.cpp
@@ -1,3 +1,11 @@
+/*CGPA Calculator 
+● Take input for the number of courses taken by the student. 
+● For each course, input the grade and the credit hours. 
+● Calculate the total credits and total grade points (grade × credit hours). 
+● Compute the GPA for the semester and then the overall CGPA. 
+● Display individual course grades and the final CGPA to the user. */
+
+    
 #include <iostream>
 #include <iomanip>
 
@@ -58,3 +66,4 @@ int main() {
 
     return 0;
 }
+
